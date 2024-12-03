1a5bf87a4f57738e7193c5899fe96a8b2dade522
Yuriy Dovzhyk <ydovzhyk@gmail.com>
2024-12-03 21:00:53 +0200

    add altternative weather icons
    add altternative weather icons

--- src/components/map/WeatherCard.js
+++ src/components/map/WeatherCard.js
@@ -1,11 +1,81 @@
+import React, { useEffect, useState } from "react";
+import {
+  WiDaySunny,
+  WiCloud,
+  WiRain,
+  WiSnow,
+  WiThunderstorm,
+  WiFog,
+} from "react-icons/wi";
 import "./WeatherMap.css";
 
-const WeatherCard = ({ temperature, icon }) => {
+const WeatherCard = () => {
+  const [weather, setWeather] = useState(null);
+  const [iconError, setIconError] = useState(false);
+
+  useEffect(() => {
+    fetchWeather();
+  }, []);
+
+  const fetchWeather = async () => {
+    try {
+      const apiKey = "1828ad4fcb19ab8b229ab979593d6cc7";
+      const city = "Reykjavik";
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+      );
+      const data = await response.json();
+      if (data) {
+        setWeather(data);
+      }
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    }
+  };
+
+  const getWeatherIcon = (main) => {
+    switch (main) {
+      case "Clear":
+        return <WiDaySunny />;
+      case "Clouds":
+        return <WiCloud />;
+      case "Rain":
+        return <WiRain />;
+      case "Snow":
+        return <WiSnow />;
+      case "Thunderstorm":
+        return <WiThunderstorm />;
+      case "Mist":
+      case "Fog":
+        return <WiFog />;
+      default:
+        return <WiDaySunny />;
+    }
+  };
+
+  if (!weather) {
+    return <div>Loading...</div>;
+  }
+
+  const { main } = weather.weather[0];
+  const temperature = weather.main.temp;
+  const iconStatic = getWeatherIcon(main);
+  const iconGoogle = `https://openweathermap.org/img/wn/${weather.weather[0]["icon"]}@2x.png`;
+
   return (
-    <div className="container">
+    <div className="container" style={{ display: weather ? "block" : "none" }}>
       <div className="card">
         <p className="temp">{Math.round(temperature)} °C</p>
-        <img className="city-icon" src={icon} alt="map" />
+        {iconError ? (
+          <div className="static-icon">{iconStatic}</div>
+        ) : (
+          <img
+            className="city-icon"
+            src={iconGoogle}
+            alt="weather icon"
+            onError={() => setIconError(true)}
+          />
+        )}
         <p className="city">Reykjavik, Iceland</p>
       </div>
     </div>
