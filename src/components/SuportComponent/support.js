a5216de54c4ecef7e91571a575abc26b85311098
iliama <ruslan.niazbaev.org@gmail.com>
2025-09-30 00:05:43 +0200

    Support component updated and ready

--- src/components/SuportComponent/support.js
+++ src/components/SuportComponent/support.js
@@ -3,70 +3,86 @@ import PropTypes from "prop-types";
 import supportIcon from "../../images/support/support.svg";
 import faqIcon from "../../images/support/faq.svg";
 import forumIcon from "../../images/support/forum.svg";
+import whatsapp from "../../images/support/whatsapp_new.svg";
 import { WithTransLate } from "../helpers/translating/index";
 import s from "./Support.module.scss";
 
 function SupportCard({ description, title, icon }) {
-return (
+  return (
     <div className={s.supportCard}>
-     <img alt="icon" className={s.icon} src={icon} />
-     <p className={s.cardTitle}>
+      <img alt="icon" className={s.icon} src={icon} />
+      <p className={s.cardTitle}>
         <WithTransLate text={title} />
-     </p>
-     <p className={s.cardText}>
+      </p>
+      <p className={s.cardText}>
         <WithTransLate text={description} />
-     </p>
+      </p>
     </div>
-);
+  );
 }
 
 SupportCard.propTypes = {
-description: PropTypes.string.isRequired,
-title: PropTypes.string.isRequired,
-icon: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
 };
 
 export default function Support() {
-return (
+  return (
     <div id="SUPPORT" className={s.support}>
-     <div className={s.sectionContent}>
+      <div className={s.sectionContent}>
+        {/* SUPPORT link updated */}
         <a
-         className={s.description}
-         href="https://bluehouseis.zohodesk.eu/portal/en/newticket?departmentId=135604000000205173&layoutId=135604000000214460"
-         target="_blank"
-         rel="noopener noreferrer"
+          className={s.description}
+          href="https://bluehouseis.zohodesk.eu/portal/en/newticket?departmentId=135604000000205173&layoutId=135604000000214460"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-         <SupportCard
-            description={"Get personal support from our team."}
+          <SupportCard
+            description={"Send us your questions"}
             title={"SUPPORT"}
             icon={supportIcon}
-         />
+          />
         </a>
+        {/* FAQ link updated */}
         <a
-         className={s.description}
-         href="https://bluehouseis.zohodesk.eu/portal/en/home"
-         target="_blank"
-         rel="noopener noreferrer"
+          className={s.description}
+          href="https://bluehouseis.zohodesk.eu/portal/en/kb/iceland"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-         <SupportCard
-            description={"Check previous guest requests."}
+          <SupportCard
+            description={"Find the answers you need"}
             title={"FAQ"}
             icon={faqIcon}
-         />
+          />
         </a>
         <a
-         className={s.description}
-         href="https://bluehouseis.zohodesk.eu/portal/en/community/guestforum"
-         target="_blank"
-         rel="noopener noreferrer"
+          className={s.description}
+          href="https://wa.me/3548647444"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-         <SupportCard
-            description={"Connect, Explore, and Share Your Journey!"}
-            title={"FORUM"}
-            icon={forumIcon}
-         />
+          <SupportCard
+            description={"Send us a message"}
+            title={"WHATSAPP"}
+            icon={whatsapp}
+            phoneNumber="+354 864 7444"
+          />
         </a>
-     </div>
+        <a
+          className={s.description}
+          href="https://bluehouseis.zohodesk.eu/portal/en/community/guestforum"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <SupportCard
+            description={"Your opinion is important to us"}
+            title={"FEEDBACK"}
+            icon={forumIcon}
+          />
+        </a>
+      </div>
     </div>
-);
+  );
 }
