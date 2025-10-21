67dfe7fd25cdc26e71f12aac1ea4992f3d150d6d
erianiel <valentina.gravil@gmail.com>
2025-10-21 16:37:09 +0200

    feat: create Newsletter component and styles and add it to HomePage

---
+++ src/components/NewsletterV2/Newsletter.jsx
@@ -0,0 +1,95 @@
+import { WithTransLate } from "../helpers/translating"
+import {useForm} from "react-hook-form"
+import useTranslateString from "../helpers/translating/useTranslateString";
+import s from "./Newsletter.module.scss"
+
+function Newsletter() {
+  const firstNamePlaceholder = useTranslateString("First name");
+  const lastNamePlaceholder = useTranslateString("Last name");
+  const emailPlaceholder = useTranslateString("Contact email");
+
+  const {
+    register, handleSubmit, formState: {errors, isSubmitting}
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  }
+
+  return (
+    <form className={s.container} onSubmit={handleSubmit(onSubmit)} noValidate>
+      <h2 className={s.title}><WithTransLate text="Sign up to the BH newsletter to get our updates from Iceland" /></h2>
+
+      <div className={s.inputWrapper}>
+        <div className={s.field}>
+          <label htmlFor="firstName" className="sr-only">First name</label>
+          <input
+            id="firstName"
+            placeholder={firstNamePlaceholder}
+            autoComplete="given-name"
+            aria-invalid={!!errors.firstName || undefined}
+            aria-describedby={errors.firstName ? "firstName-error" : undefined}
+            {...register("firstName", { required: "First name is required" })}
+            className={errors.firstName ? `${s.input} ${s.inputError}` : s.input}
+          />
+          <p
+            id="firstName-error"
+            role={errors.firstName ? "alert" : undefined}
+            className={errors.firstName ? s.errorMessage : s.errorSpace}
+          >
+            {errors.firstName ? errors.firstName.message : "\u00a0"}
+          </p>
+        </div>
+
+        <div className={s.field}>
+          <label htmlFor="lastName" className="sr-only">Last name</label>
+          <input
+            id="lastName"
+            placeholder={lastNamePlaceholder}
+            autoComplete="family-name"
+            aria-invalid={!!errors.lastName || undefined}
+            aria-describedby={errors.lastName ? "lastName-error" : undefined}
+            {...register("lastName", { required: "Last name is required" })}
+            className={errors.lastName ? `${s.input} ${s.inputError}` : s.input}
+          />
+          <p
+            id="lastName-error"
+            role={errors.lastName ? "alert" : undefined}
+            className={errors.lastName ? s.errorMessage : s.errorSpace}
+          >
+            {errors.lastName ? errors.lastName.message : "\u00a0"}
+          </p>
+        </div>
+
+        <div className={s.field}>
+          <label htmlFor="email" className="sr-only">Email address</label>
+          <input
+            id="email"
+            type="email"
+            placeholder={emailPlaceholder}
+            autoComplete="email"
+            inputMode="email"
+            aria-invalid={!!errors.email || undefined}
+            aria-describedby={errors.email ? "email-error" : undefined}
+            {...register("email", {
+              required: "Email is required",
+              pattern: { value: /^\S+@\S+$/, message: "Enter a valid email" }
+            })}
+            className={errors.email ? `${s.input} ${s.inputError}` : s.input}
+          />
+          <p
+            id="email-error"
+            role={errors.email ? "alert" : undefined}
+            className={errors.email ? s.errorMessage : s.errorSpace}
+          >
+            {errors.email ? errors.email.message : "\u00a0"}
+          </p>
+        </div>
+
+        <button type="submit" disabled={isSubmitting} className={s.submit}><WithTransLate text="Sign up" /></button>
+      </div>
+    </form>
+  )
+}
+
+export default Newsletter
