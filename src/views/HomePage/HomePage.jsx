e6a8e0c0c3f682ed4491f0ff1845f69c7238f66f
erianiel <valentina.gravil@gmail.com>
2025-10-27 11:52:36 +0100

    chore: remove old newsletter component

--- src/views/HomePage/HomePage.jsx
+++ src/views/HomePage/HomePage.jsx
@@ -13,7 +13,7 @@ import FollowUs from "../../components/FollowUs/FollowUs.jsx";
 
 import s from "./HomePage.module.scss";
 import ReviewSection from "../../components/ReviewSlider/ReviewSection.jsx";
-import Newsletter from "../../components/NewsletterV2/Newsletter.jsx";
+import Newsletter from "../../components/Newsletter/Newsletter.jsx";
 
 const GALLERY_BACKGROUND_IMAGES_AND_TITLES = [
   { background: roomsImage, title: "Rooms" },
