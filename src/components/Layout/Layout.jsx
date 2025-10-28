3cab4c60b3e49f2d845620c3a47f121e964c3dbc
erianiel <valentina.gravil@gmail.com>
2025-10-28 15:54:39 +0100

    fix: conditionally render NewMap to match Figma mobile layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -4,14 +4,16 @@ import CookiesBanner from "../CookiesBanner/CookiesBanner.js";
 import Footer from "../Footer/Footer.jsx";
 
 import s from "./Layout.module.scss";
+import useBreakpointsNew from "../../Styles/useBreakpointsNew.js";
 
 function Layout({children}) {
+  const {isSmallScreen, isDesktop} = useBreakpointsNew();
 
   return (
     <div className={s.container}>
       <Header />
       <div className={s.childrenWrap}>{children}</div>
-      <div className={s.fullWidthSection}><NewMap /></div>
+      {(isSmallScreen || isDesktop) && <div className={s.fullWidthSection}><NewMap /></div>}
       <CookiesBanner />
       <Footer />
     </div>
