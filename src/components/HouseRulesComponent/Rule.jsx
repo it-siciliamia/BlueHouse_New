73993fd315e67913325034eb3bf3ce0f20244486
AKavaa <alex.kavaleuskiy33@gmail.com>
2025-12-15 12:05:21 +0200

    fix Rule.jsx structure

--- src/components/HouseRulesComponent/Rule.jsx
+++ src/components/HouseRulesComponent/Rule.jsx
@@ -7,19 +7,17 @@ function Rule({ Icon, header, text }) {
   return (
     <div className={s.rule}>
       {!!Icon && <img src={Icon} className={s.iconStyle} alt="Rule" />}
-      <p className={s.ruleText}>
+      <div className={s.ruleText}>
         <div className={s.ruleContent}>
-
-
         {header && (
           <h3 className={s.ruleHeader}>
-
            <WithTransLate text={header} />
-           </h3> // section to insert headers in the Guidelanes.jsx
+           </h3>
         )}
+        {/* section to insert headers in the Guidelines.jsx */}
     </div>
         <WithTransLate text={text} />
-      </p>
+      </div>
     </div>
   );
 }
