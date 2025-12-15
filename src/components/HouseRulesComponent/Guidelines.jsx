a7930481386deaefdc4fc54a01eaa783e68982b0
AKavaa <alex.kavaleuskiy33@gmail.com>
2025-12-15 10:57:36 +0200

    added the correct icons based on new figma design

--- src/components/HouseRulesComponent/Guidelines.jsx
+++ src/components/HouseRulesComponent/Guidelines.jsx
@@ -5,28 +5,28 @@ function Guidelines() {
   return (
     <div className={s.guidelines}>
       <Rule
-        Icon={"./1.svg"}
-        text="Do not smoke inside the room and the house. 
+        Icon={"./src/images/nosmoking.svg"}
+        text="Do not smoke inside the room and the house.
         It is not allowed by the icelandic law and there is a penalty for people who do not adhere to the these rules."
       />
       <Rule
-        Icon={"./2.svg"}
-        text="Respect the quiet house. 
-        As we have a lot of guests of different age groups, we ask you to respect that some people are going to bed early as they are waking up early. 
+        Icon={"./src/images/quiet.svg"}
+        text="Respect the quiet house.
+        As we have a lot of guests of different age groups, we ask you to respect that some people are going to bed early as they are waking up early.
         Please try to keep it down between 10pm and 7am."
       />
       <Rule
-        Icon={"./3.svg"}
-        text="Do not leave the bathroom dirty after using it. 
+        Icon={"./src/images/bathroom.svg"}
+        text="Do not leave the bathroom dirty after using it.
         Please keep the toilet in the way you found it. We all benefit from it and it will feel more like home when we all play our role in it."
       />
       <Rule
-        Icon={"./4.svg"}
-        text="Do not throw anything into the toilet other than toilet paper. 
+        Icon={"./src/images/toiletpaper.svg"}
+        text="Do not throw anything into the toilet other than toilet paper.
         We do not want the toilet to clog. In all bathrooms we provide rubbish bins where you can throw in all other kinds of trash."
       />
       <Rule
-        Icon={"./5.svg"}
+        Icon={"./src/images/breakfast.svg"}
         text="Be sure to have breakfast included before taking it from the kitchen or the fridge.
                                                 Breakfast time is between 8 and 10:30 am. For direct bookers we offer breakfast for free.
                                                 Depending on the room you book, you will receive self-service cereal or continental
@@ -34,17 +34,17 @@ function Guidelines() {
                                                 you wish to add it to your booking and we will send you the pay link."
       />
       <Rule
-        Icon={"./6.svg"}
+        Icon={"./src/images/kitchen.svg"}
         text="Do not make a big mess in the kitchen when you use it since you are not alone in the house. Always keep in mind how you would like to find the kitchen when entering the room. This is how we would like you to leave it too."
       />
       <Rule
-        Icon={"./8.svg"}
+        Icon={"./src/images/light.svg"}
         text="Please do not leave the lights and the heater on when it is not necessary - we are an eco-friendly house."
       />
       <Rule
-        Icon={"./9.svg"}
-        text="Check-out is until 11am and please leave the key on the door of your room before you depart. 
-        If you wish to check out later, please contact info@bluehouse.is as we can help you further. 
+        Icon={"./src/images/check-in-out.svg"}
+        text="Check-out is until 11am and please leave the key on the door of your room before you depart.
+        If you wish to check out later, please contact info@bluehouse.is as we can help you further.
         You are also always welcome to stay a couple of more hours in our common areas if needed."
       />
     </div>
