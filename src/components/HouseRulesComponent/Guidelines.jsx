8d2a37372fab20677d323d450c8c5c8c2c091587
AKavaa <alex.kavaleuskiy33@gmail.com>
2025-12-15 11:30:57 +0200

    added the correct headers for the paragraphs

--- src/components/HouseRulesComponent/Guidelines.jsx
+++ src/components/HouseRulesComponent/Guidelines.jsx
@@ -12,22 +12,27 @@ function Guidelines() {
         It is not allowed by the Icelandic law and there is a penalty for people who don’t adhere to the these rules."
       />
       <Rule
+
         Icon={"./src/images/quiet.svg"}
+        header="Keep the noise to a minimum"
         text="We ask you to respect that some guests go to bed early as they wake up early.
          Please try to keep it down between 10pm and 7am  to respect our guests' sleep."
       />
       <Rule
         Icon={"./src/images/bathroom.svg"}
+        header="Clean after youself"
         text="Please leave the bathroom clean after using it.
         By doing so, we all contribute to a more pleasant environment that feels more like home for everyone."
       />
       <Rule
         Icon={"./src/images/toiletpaper.svg"}
+        header="Avoid clogs"
         text="Please throw only toilet paper in the toilet.
         Use rubbish bins for other type of rubbish to avoid clogs."
       />
       <Rule
         Icon={"./src/images/breakfast.svg"}
+        header="Book your breakfast"
         text="Have breakfast included before taking it from the kitchen.
         Breakfast is from 8 to 10:30 am. Direct bookers get free continental breakfast.
         Channel bookers will have to pay 10€ per person per night for their breakfast.
@@ -35,15 +40,18 @@ function Guidelines() {
       />
       <Rule
         Icon={"./src/images/kitchen.svg"}
+        header="Keep the kitchen tidy"
         text="Please keep the kitchen tidy when using it.
         Remember how you want to find it when you enter. We kindly ask you to leave it the same way."
       />
       <Rule
         Icon={"./src/images/light.svg"}
+        header="Use electricity responsibly"
         text="Please do not leave the lights and the heater on when it is not necessary - we are an eco-friendly house."
       />
       <Rule
         Icon={"./src/images/check-in-out.svg"}
+        header="Check in & check out"
         text="Two days before your arrival, you will receive an email with the information to check in from 4 pm.
           Check-out is until 11am. Please leave the key on your door before leaving.
           Contact info@bluehouse.is for late check-out.
