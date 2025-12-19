d09de09b845b4d73df927fc2c48e70372bda5a0c
SHfrontend6Siciliamia <frontend6@siciliamia.com>
2025-12-19 11:03:35 -0300

    feat: add HeaderOnlyLayout component for pages displaying only a header

--- src/components/Layout/HeaderOnlyLayout.jsx
+++ src/components/Layout/HeaderOnlyLayout.jsx
@@ -3,15 +3,13 @@ import { Outlet } from "react-router-dom";
 import s from "./Layout.module.scss";
 import Header from "../header/Header.jsx";
 
-function HeaderOnlyLayout() {
+function HeaderOnlyLayout({ children }) {
   return (
     <>
       <div className={s.header}>
         <Header />
       </div>
-      <div className={s.outlet}>
-        <Outlet />
-      </div>
+      <div className={s.outlet}>{children ? children : <Outlet />}</div>
     </>
   );
 }
