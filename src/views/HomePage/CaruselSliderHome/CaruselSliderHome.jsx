7a3fe47cd0f2dc5ee6f03e766eb370e10a0135e0
erianiel <valentina.gravil@gmail.com>
2026-01-07 11:48:18 +0100

    refactor: replace custom placeholder with video poster

--- src/views/HomePage/CaruselSliderHome/CaruselSliderHome.jsx
+++ src/views/HomePage/CaruselSliderHome/CaruselSliderHome.jsx
@@ -1,17 +1,14 @@
 import { useState, useEffect, useRef } from "react";
-import { useSelector, useDispatch } from "react-redux";
 
 import s from "./CaruselSliderHome.module.scss";
 import IconButton from "../../../components/Shared/ui/IconButton.jsx";
-import placeholder from "../../../images/homePageSlider/placeholder.webp";
+import posterSrc from "../../../images/homePageSlider/intro-poster.jpg";
 import slide01 from "../../../images/homePageSlider/slide1.png";
 import slide02 from "../../../images/homePageSlider/slide2.png";
 import slide03 from "../../../images/homePageSlider/slide3.png";
 import slide04 from "../../../images/homePageSlider/slide4.png";
 import slide05 from "../../../images/homePageSlider/slide5.png";
 import slide06 from "../../../images/homePageSlider/slide6.png";
-import { getIsPlaceholderShown } from "../../../redux/technitial/technical-selectors.js";
-import { setPlaceholderShown } from "../../../redux/technitial/technical-slice.js";
 import videoSrc from "../../../videos/intro.mp4";
 
 const images = [videoSrc, slide01, slide02, slide03, slide04, slide05, slide06];
@@ -25,12 +22,7 @@ const texts = [
 ];
 
 export default function CaruselSliderHome() {
-  const isPlaceholderShown = useSelector(getIsPlaceholderShown);
-  const dispatch = useDispatch();
-
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [showPlaceholder, setShowPlaceholder] = useState(true);
-  const [fadeOutPlaceholder, setFadeOutPlaceholder] = useState(false);
   const videoRef = useRef(null);
   const intervalRef = useRef(null);
 
@@ -61,26 +53,6 @@ export default function CaruselSliderHome() {
       }
     };
   }, [currentIndex]);
-
-  useEffect(() => {
-    const handleCanPlayThrough = () => {
-      setFadeOutPlaceholder(true);
-      setTimeout(() => {
-        dispatch(setPlaceholderShown(true));
-        setShowPlaceholder(false);
-      }, 2000);
-    };
-
-    if (videoRef.current) {
-      videoRef.current.addEventListener("canplaythrough", handleCanPlayThrough);
-    }
-
-    return () => {
-      if (videoRef.current) {
-        videoRef.current.removeEventListener("canplaythrough", handleCanPlayThrough);
-      }
-    };
-  }, [dispatch]);
 
   const goToPreviousSlide = () => {
     setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
@@ -124,13 +96,6 @@ export default function CaruselSliderHome() {
         onClick={goToPreviousSlide}
         className={s.arrowButtonLeft}
       />
-      {!!showPlaceholder && !isPlaceholderShown && (
-        <img
-          src={placeholder}
-          alt="Placeholder"
-          className={`${s.placeholder} ${fadeOutPlaceholder ? s.fadeOut : ""}`}
-        />
-      )}
       {images.map((image, index) =>
         <div key={index} className={s.slideContainer}>
           {index === 0 ? (
@@ -139,6 +104,7 @@ export default function CaruselSliderHome() {
               className={`${s.image} ${index === currentIndex ? s.currentImage : ""}`}
               style={{ display: index === currentIndex ? "block" : "none" }}
               src={image}
+              poster={posterSrc}
               muted
             />
           ) : (
